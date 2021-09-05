7702b0a40066f066711c61cf78835f3591247449
jh2856 <jh2856@vt.edu>
2021-09-04 21:32:12 -0400

    Issue 4

---
+++ idea_manager/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from idea_manager.models import DateIdea
+
+class DateIdeaSerializer(serializers.ModelSerializer):
+
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    created_date = serializers.DateTimeField()
+    description = serializers.CharField()
+    location = serializers.CharField()
+
+    class Meta:
+        model = DateIdea
+        fields = '__all__'
+    
