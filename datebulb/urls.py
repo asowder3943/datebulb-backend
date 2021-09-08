c8a6d7fba006dea5beb3e428d00cfd2f2d37c6b6
Adam Sowder <asowder3943@gmail.com>
2021-09-08 11:48:52 -0400

    Addressing issues 12 and 17

--- datebulb/urls.py
+++ datebulb/urls.py
@@ -1,15 +1,18 @@
 from django.urls import include, path
 from rest_framework import routers
-from user_manager import views as user_views
+from user_manager.views import *
 from idea_manager import views as idea_views
 
 router = routers.DefaultRouter()
-router.register(r'users', user_views.UserViewSet)
+router.register(r'users', UserViewSet)
 router.register(r'dateideas', idea_views.DateIdeaViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('set-csrf/', set_csrf_token, name='Set-CSRF'),
+    path('login/', login_view, name='Login'),
+    path('test-auth/', CheckAuth.as_view(), name='check-auth')
 ]
--- user_manager/serializers.py
+++ user_manager/serializers.py
@@ -2,8 +2,23 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+
+    # take plain text as input, but store passwords as hashes
+    def create(self, validated_data):
+        if "password" in validated_data:
+            from django.contrib.auth.hashers import make_password
+            validated_data["password"] = make_password(
+                validated_data["password"])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if "password" in validated_data:
+            from django.contrib.auth.hashers import make_password
+            validated_data["password"] = make_password(
+                validated_data["password"])
+        return super().update(instance, validated_data)
+
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'password']
-
+        fields = '__all__'
