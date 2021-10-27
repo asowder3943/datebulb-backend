3e2231babd6a2ab3bef61dea6f05623495c57ef7
Adam Sowder <asowder3943@gmail.com>
2021-10-27 18:41:01 -0400

    added nested serializer

--- event_manager/serializers.py
+++ event_manager/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from event_manager.models import Event
+from idea_manager.serializers import DateIdeaSerializer
 
 
 class EventSerializer(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
-    ideas = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    ideas = DateIdeaSerializer(many=True, read_only=True)
     start_time = serializers.DateTimeField()
     end_time = serializers.DateTimeField()
     participants = serializers.PrimaryKeyRelatedField(
